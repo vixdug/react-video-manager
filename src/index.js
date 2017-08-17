7f202237e698248c204a2bd23accf8ef354878d6
Victoria Duggan <victoria.duggan@shopify.com>
2017-08-17 12:48:20 -0600

    adds layout and search functionality, integrates youtube API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,54 @@
-import React from 'react';
+import _ from 'lodash'
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
+import YTSearch from 'youtube-api-search';
+import VideoDetail from './components/video_details';
+const API_KEY = 'AIzaSyBv9n7B1BMLoXm305SRVACiDtWcFTaiFoE';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// creates a new component - component produces HTML
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+this.videoSearch('beaver eating wood')
+
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+    });
+  }
+
+  render() {
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+  return  (
+    <div>
+      <SearchBar onSearchTermChange={videoSearch}/>
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList
+      onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+      videos={this.state.videos}/>
+   </div>
+    );
+  }
+}
+
+// take this components generated HTML and put it on the page (in the DOM)
+
+
+ReactDOM.render(<App />, document.querySelector('.container'))
